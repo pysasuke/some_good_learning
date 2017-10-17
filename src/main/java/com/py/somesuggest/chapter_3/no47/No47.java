c9239a9275b263ca05e8af8324382d27b4ecb896
pysasuke <pysasuke@outlook.com>
2017-10-17 23:12:27 +0800

    代码提交：47-49条

---
+++ src/main/java/com/py/somesuggest/chapter_3/no47/No47.java
@@ -0,0 +1,76 @@
+package com.py.somesuggest.chapter_3.no47;
+
+/**
+ * 在equals中使用getClass进行类型判断
+ * Created by Administrator on 2017/10/17 0017.
+ */
+//在覆写equals时建议使用getClass进行类型判断，而不要使用instanceof
+public class No47 {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("张三 ", 100);
+        Employee e2 = new Employee("张三 ", 1001);
+        Person p1 = new Person("张三 ");
+        System.out.println(p1.equals(e1));    //true
+        System.out.println(p1.equals(e2));    //true
+        System.out.println(e1.equals(p1));    //false
+        System.out.println(e1.equals(e2));    //false
+    }
+}
+
+class Person {
+    private String name;
+
+    public Person(String _name) {
+        name = _name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+//        if (obj instanceof Person) {    //NOT DO THIS  违反了equals的传递性原则：对于实例对象x、y、z来说，如果x.equals(y)返回true，y.equals(z)返回true，那么x.equals(z)也应该返回true。
+        if (null != obj && obj.getClass() == this.getClass()) {
+            Person p = (Person) obj;
+            if (p.getName() == null || name == null) {
+                return false;
+            } else {
+                return name.equalsIgnoreCase(p.getName());
+            }
+        }
+        return false;
+    }
+}
+
+class Employee extends Person {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Employee(String _name, int _id) {
+        super(_name);
+        id = _id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+//        if (obj instanceof Employee) {
+        if (null != obj && obj.getClass() == this.getClass()) {
+            Employee e = (Employee) obj;
+            return super.equals(obj) && e.getId() == id;
+        }
+        return false;
+    }
+
+}
---
+++ src/main/java/com/py/somesuggest/chapter_3/no48/No48.java
@@ -0,0 +1,75 @@
+package com.py.somesuggest.chapter_3.no48;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 覆写equals方法必须覆写hashCode方法
+ * Created by Administrator on 2017/10/17 0017.
+ */
+/*
+对象元素的hashCode方法返回值：
+    它是一个对象的哈希码，是由Object类的本地方法生成的，确保每个对象有一个哈希码(这也是哈希算法的基本要求：
+    任意输入k，通过一定算法f(k)，将其转换为非可逆的输出，对于两个输入k1和k2，若要求k1=k2，
+    则必须f(k1)=f(k2)，但也允许k1!=k2,f(k1)=f(k2)的情况存在)。
+ */
+public class No48 {
+    public static void main(String[] args) {
+        //Person类的实例作为Map的key
+        Map<Person, Object> map = new HashMap<Person, Object>() {
+            {
+                put(new Person("张三"), new Object());
+            }
+        };
+        //Person类的实例作为List的元素
+        List<Person> list = new ArrayList<Person>() {
+            {
+                add(new Person("张三"));
+            }
+        };
+        //列表中是否包含
+        boolean b1 = list.contains(new Person("张三"));
+        boolean b2 = map.containsKey(new Person("张三"));
+        System.out.println(b1);    //true
+        System.out.println(b2);    //false
+    }
+}
+
+class Person {
+    private String name;
+
+    public Person(String _name) {
+        name = _name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (null != obj && obj.getClass() == this.getClass()) {
+            Person p = (Person) obj;
+            if (p.getName() == null || name == null) {
+                return false;
+            } else {
+                return name.equalsIgnoreCase(p.getName());
+            }
+        }
+        return false;
+    }
+
+    //重写hashCode方法
+    @Override
+    public int hashCode(){
+        return new HashCodeBuilder().append(name).toHashCode();
+    }
+}
