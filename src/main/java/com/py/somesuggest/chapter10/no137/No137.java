edb4b805416e90b5b3ac6a8464cb62b5176758c1
pysasuke <pysasuke@outlook.com>
2017-11-23 22:21:19 +0800

    代码提交：137-138

---
+++ src/main/java/com/py/somesuggest/chapter10/no137/No137.java
@@ -0,0 +1,14 @@
+package com.py.somesuggest.chapter10.no137;
+
+/**
+ * 调整JVM参数以提升性能
+ * Created by Administrator on 2017/11/23 0023.
+ */
+/*
+1.调整堆内存大小
+2.调整堆内存中各分区的比例
+3.变更GC的垃圾回收策略
+4.更换JVM
+ */
+public class No137 {
+}
